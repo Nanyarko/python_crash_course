d592169b045ab32332338acd5eb64291128e9aa6
Nanyarko <arkohjames111@gmail.com>
2023-07-03 09:30:40 -1200

    create data_type

---
+++ data_types.py
@@ -0,0 +1,14 @@
+name= ("santa")
+age={2023-2003}
+date_of_birth=age
+print(f"My name is {name.upper()} and i was born in {date_of_birth} hence i'm {age} years old")
+
+website= "www.google.com"
+print(website.removeprefix("www."))
+
+a,b,c=3,5,7
+print(a+b+c)# addition of a,b,c 
+print(a**5)# 3 to the power 5
+print(b**5)# 5 to the power 5
+print(c**5)# 7 to the power 5
+
--- new.py
+++ new.py
@@ -1,3 +1,5 @@
-message = "Hello "
-name = "James"
-print(message + name)
+first_name='Nana'
+last_name='nyarko'
+middle_name='kwesi'
+full_name=f'{first_name}  {last_name} '
+print(full_name)
