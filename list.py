55d25074a1576909f8b32c32b0451b2e559be5ac
Nanyarko <arkohjames111@gmail.com>
2023-07-09 14:22:31 -1200

    Create list

---
+++ list.py
@@ -0,0 +1,9 @@
+Courses = ['communication','architecture','physics for cs','probability','security policies','ethical aspect','report writting']
+print(Courses[4])
+print(Courses[5])
+print(Courses[6])
+print(f"one of my courses this sem is {Courses[6].title()}")
+Courses.append("python")
+print(Courses)
+Courses.sort()
+print(Courses)
