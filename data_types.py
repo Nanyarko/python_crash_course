3b5fedfc4b47ce38f150c4896bc0f7b7c8f3f7ce
Nanyarko <arkohjames111@gmail.com>
2023-07-09 13:12:19 -1200

    Update data_types

--- data_types.py
+++ data_types.py
@@ -1,14 +1,11 @@
-name= ("santa")
-age={2023-2003}
-date_of_birth=age
-print(f"My name is {name.upper()} and i was born in {date_of_birth} hence i'm {age} years old")
-
-website= "www.google.com"
+name = "santa"
+age = 20
+date_of_birth = 2023-age
+print(f"My name is {name.upper()} and I was born in {date_of_birth} hence i'm {age} years old")
+website = "www.google.com"
 print(website.removeprefix("www."))
-
-a,b,c=3,5,7
-print(a+b+c)# addition of a,b,c 
+a,b,c = 3,5,7
+print(a + b + c)# addition of a,b,c 
 print(a**5)# 3 to the power 5
 print(b**5)# 5 to the power 5
 print(c**5)# 7 to the power 5
-
